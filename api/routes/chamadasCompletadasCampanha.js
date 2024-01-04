1eb93a8368d32baa78d61ecc7edf9bf9819ece08
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-04 13:30:29 -0300

    adicionando rotas

---
+++ api/routes/chamadasCompletadasCampanha.js
@@ -0,0 +1,32 @@
+module.exports = async function(app){
+  app.get('/chamadasCompletadasCampanha', function(req,res){  
+    require('dotenv').config();
+    const http = require("https");
+
+    const options = {
+      "method": "GET",
+      "hostname": "groscon.callix.com.br",
+      "port": null,
+      "path": "/api/v1/campaign_completed_calls?filter[started_at]=2023-12-21T00:00:00.000Z,2023-12-21T23:59:59.999Z",
+      "headers": {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" + '' + process.env.TOKEN_CALLIX
+      }
+    };
+
+    req = http.request(options, function (res) {
+      const chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        const body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    req.end();
+  });
+  };
---
+++ api/routes/contatosDeCampanha.js
@@ -0,0 +1,31 @@
+module.exports = async function(app){
+    app.get('/contatosDeCampanha', function(req,res){  
+    const http = require("https");
+  
+      const options = {
+        "method": "GET",
+        "hostname": "groscon.callix.com.br",
+        "port": null,
+        "path": "/api/v1/campaign_contacts?filter[campaign_id]=10",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer" + '' + process.env.TOKEN_CALLIX
+        }
+      };
+  
+      req = http.request(options, function (res) {
+        const chunks = [];
+  
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+  
+        res.on("end", function () {
+          const body = Buffer.concat(chunks);
+          console.log(body.toString());
+        });
+      });
+  
+      req.end();
+    });
+    };
