59a1bd2c1874d72b1780e8d9a15517db596146d4
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-04 11:07:20 -0300

    Inicio

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const consign = require('consign');
+
+const app = express();
+
+app.listen(3000,function(){
+    console.log('App online');
+}),
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+consign()
+	.include('api/routes')
+	.then('api/models')
+	.then('api/controllers')
+	.into(app);
+
+module.exports = app;
