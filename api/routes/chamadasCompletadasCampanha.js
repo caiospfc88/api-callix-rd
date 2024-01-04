7ceb217317c64d3bee0802ffa00c216dac86a3ca
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-04 16:58:12 -0300

    correção em rotas

--- api/routes/chamadasCompletadasCampanha.js
+++ api/routes/chamadasCompletadasCampanha.js
@@ -2,7 +2,7 @@ module.exports = async function(app){
   app.get('/chamadasCompletadasCampanha', function(req,res){  
     require('dotenv').config();
     const http = require("https");
-
+    const token = process.env.TOKEN_CALLIX
     const options = {
       "method": "GET",
       "hostname": "groscon.callix.com.br",
@@ -10,7 +10,7 @@ module.exports = async function(app){
       "path": "/api/v1/campaign_completed_calls?filter[started_at]=2023-12-21T00:00:00.000Z,2023-12-21T23:59:59.999Z",
       "headers": {
         "Content-Type": "application/json",
-        "Authorization": "Bearer" + '' + process.env.TOKEN_CALLIX
+        "Authorization": token
       }
     };
 
--- api/routes/contatosDeCampanha.js
+++ api/routes/contatosDeCampanha.js
@@ -1,20 +1,19 @@
 module.exports = async function(app){
     app.get('/contatosDeCampanha', function(req,res){  
-    const http = require("https");
-    const token = process.env.TOKEN_CALLIX
+      require('dotenv').config();
+      const http = require("https");
+      const token = process.env.TOKEN_CALLIX
   
       const options = {
         "method": "GET",
         "hostname": "groscon.callix.com.br",
         "port": null,
-        "path": "/api/v1/campaign_contacts?filter[campaign_id]=10",
+        "path": "/api/v1/campaign_contacts?filter[campaign_id]=11",
         "headers": {
           "Content-Type": "application/json",
           "Authorization": token
         }
       };
-
-      console.log(options);
   
       req = http.request(options, function (res) {
         const chunks = [];
