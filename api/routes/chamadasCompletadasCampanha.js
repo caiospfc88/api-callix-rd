a77cdf422a8a26cea5385d47fc3157623cc05b88
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-10 17:53:53 -0300

    acerto de rotas

--- api/routes/chamadasCompletadasCampanha.js
+++ api/routes/chamadasCompletadasCampanha.js
@@ -3,17 +3,21 @@ module.exports = async function(app){
     require('dotenv').config();
     const http = require("https");
     const token = process.env.TOKEN_CALLIX
+    var idCampanha = req.query.id;
+    let dth_inicial = req.query.dth_inicial;
+    let dth_final = req.query.dth_final;
+
     const options = {
       "method": "GET",
       "hostname": "groscon.callix.com.br",
       "port": null,
-      "path": "/api/v1/campaign_completed_calls?filter[started_at]=2023-12-21T00:00:00.000Z,2023-12-21T23:59:59.999Z",
+      "path": "/api/v1/campaign_completed_calls?filter[started_at]="+dth_inicial+"T00:00:00.000Z,"+dth_final+"T23:59:59.999Z&filter[campaign_id]="+ idCampanha,
       "headers": {
         "Content-Type": "application/json",
         "Authorization": token
       }
     };
-
+    console.log(options);
     req = http.request(options, function (res) {
       const chunks = [];
 
