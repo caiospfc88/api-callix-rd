448fe8760f75b39b92c6945ef538093fdb05e2be
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-11 17:57:06 -0300

    criação de rota

---
+++ api/routes/operadores.js
@@ -0,0 +1,34 @@
+module.exports = async function(app){
+    app.get('/operadores', function(req,res1){  
+      require('dotenv').config();
+      const http = require("https");
+      const token = process.env.TOKEN_CALLIX
+       
+      const options = {
+        "method": "GET",
+        "hostname": "groscon.callix.com.br",
+        "port": null,
+        "path": "/api/v1/users",
+        "headers": {
+          "Content-Type": "application/json",
+          "Authorization": token
+        }
+      };
+
+      req = http.request(options, function (res) {
+        const chunks = [];
+  
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+  
+        res.on("end", function () {
+          const body = Buffer.concat(chunks);
+          const dados = body.toString();
+          res1.send(dados);
+        });
+        
+      });
+      req.end();
+    });
+    };
