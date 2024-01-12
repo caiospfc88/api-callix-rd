1e4d423096ef3a5e3534b0a8b788b4b4da67fd36
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-12 11:00:53 -0300

    criação das rotas

---
+++ api/routes/chamadasNaoCompletadas.js
@@ -0,0 +1,37 @@
+module.exports = async function(app){
+  app.get('/chamadasNaoCompletadas', function(req,res1){  
+    require('dotenv').config();
+    const http = require("https");
+    const token = process.env.TOKEN_CALLIX
+    var idCampanha = req.query.id;
+    let dth_inicial = req.query.dth_inicial;
+    let dth_final = req.query.dth_final;
+
+    const options = {
+      "method": "GET",
+      "hostname": "groscon.callix.com.br",
+      "port": null,
+      "path": "/api/v1/campaign_missed_calls?filter[started_at]="+dth_inicial+"T00:00:00.000Z,"+dth_final+"T23:59:59.999Z&filter[campaign]="+ idCampanha,
+      "headers": {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    };
+    console.log(options);
+    req = http.request(options, function (res) {
+      const chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        const body = Buffer.concat(chunks);
+        const dados = body.toString();
+        res1.send(dados);
+      });
+      
+    });
+    req.end();
+  });
+  };
