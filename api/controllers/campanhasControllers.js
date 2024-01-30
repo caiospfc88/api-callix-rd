4722e919c6ab6bcb0749fc54407fc7f0e90b8d25
caiospfc88 <49125995+caiospfc88@users.noreply.github.com>
2024-01-30 17:58:15 -0300

    criação de função para limpar lixo da resposta de campanha

--- api/controllers/campanhasControllers.js
+++ api/controllers/campanhasControllers.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const token = process.env.TOKEN_CALLIX;
 const axios = require("axios");
+const { json } = require('body-parser');
 
 let dados = [];
 
@@ -12,18 +13,28 @@ module.exports.retornaArrayCampanha = async function (app,req,res){
         headers: {
           'Content-Type': 'application/json',
           Authorization: token
-        }
+        },
+        transformResponse : [
+          function (data) {
+            return JSON.parse(data)
+              .map(i => ({
+                id : i.id,
+                nome : i.attributes.name
+              }));           
+          }
+        ]
     };
-    console.log(options);
+    console.log()
+    //console.log(options);
     await axios.request(options).then(function (response) {
-      //console.log(response.data);
+    console.log(response.data);
     dados = response.data.data;
       
       
     }).catch(function (error) {
       console.error(error);
     });
-    console.log(dados);
+    //console.log(dados);
     
     res.send(dados);
     
